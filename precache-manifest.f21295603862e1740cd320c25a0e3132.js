aaee4296581beee6a73e090554e1f457bba51465
Parth Gupta <parth1716@gmail.com>
2020-08-04 02:20:35 +0530

    Updates

---
+++ precache-manifest.f21295603862e1740cd320c25a0e3132.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e416faced5d4bc015a3d01a4ac6949ab",
+    "url": "/keeper-app/index.html"
+  },
+  {
+    "revision": "5a9b0468e6f58f9ceedb",
+    "url": "/keeper-app/static/css/2.d0b9e7a5.chunk.css"
+  },
+  {
+    "revision": "a3d23738aebbd93ac668",
+    "url": "/keeper-app/static/css/main.fc6f8b3c.chunk.css"
+  },
+  {
+    "revision": "5a9b0468e6f58f9ceedb",
+    "url": "/keeper-app/static/js/2.0c47f629.chunk.js"
+  },
+  {
+    "revision": "34ce7cc2eb31f45123cdbd78ce8700d3",
+    "url": "/keeper-app/static/js/2.0c47f629.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3d23738aebbd93ac668",
+    "url": "/keeper-app/static/js/main.597740d8.chunk.js"
+  },
+  {
+    "revision": "043c4bc9b45c8772bfac",
+    "url": "/keeper-app/static/js/runtime-main.8929710f.js"
+  },
+  {
+    "revision": "62faadd41f651c5b4b12fcd26dbcbb19",
+    "url": "/keeper-app/static/media/access-anywhere-illustration.62faadd4.jpg"
+  },
+  {
+    "revision": "423311166b7c9a45447c35dae3723ac5",
+    "url": "/keeper-app/static/media/format-illustration.42331116.png"
+  },
+  {
+    "revision": "66af99ebdf3bf25394651cbb655a9199",
+    "url": "/keeper-app/static/media/important-illustration.66af99eb.jpg"
+  },
+  {
+    "revision": "e61469021eac863a4f4b7844257706db",
+    "url": "/keeper-app/static/media/notes-illustrations.e6146902.jpg"
+  },
+  {
+    "revision": "b405f38999478827fc54e7e01c75e5b0",
+    "url": "/keeper-app/static/media/pass-protection-illustration.b405f389.jpg"
+  }
+]);
